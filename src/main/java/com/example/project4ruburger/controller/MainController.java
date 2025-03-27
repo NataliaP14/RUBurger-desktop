7523152a5b34577e3688b85bc3ce39635d154896
Natalia Peguero <pegueronatalia@gmail.com>
2025-03-27 18:39:31 -0400

    set up the views and made the buttons interactable yassss frontend

--- src/main/java/com/example/project4ruburger/controller/MainController.java
+++ src/main/java/com/example/project4ruburger/controller/MainController.java
@@ -1,17 +1,81 @@
 package com.example.project4ruburger.controller;
 
+import javafx.animation.ScaleTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 /**
  * @author Natalia Peguero, Olivia Kamau
  */
 public class MainController {
-	@FXML
-	private Label welcomeText;
+
+	@FXML private Button cart;
+	@FXML private Button orders;
+	@FXML private StackPane burgerBox;
+	@FXML private StackPane sandwichBox;
+	@FXML private StackPane beverageBox;
+	@FXML private StackPane sideBox;
+
+
+	private void hover(StackPane stackPane) {
+		ScaleTransition in = new ScaleTransition(Duration.millis(300), stackPane);
+		in.setToX(1.05);
+		in.setToY(1.05);
+
+		ScaleTransition out = new ScaleTransition(Duration.millis(300), stackPane);
+		out.setToX(1.0);
+		out.setToY(1.0);
+
+		stackPane.setOnMouseEntered(e -> in.playFromStart());
+		stackPane.setOnMouseExited(e -> out.playFromStart());
+	}
+
+	private void loadScene(String file, String title) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/project4ruburger/" + file));
+			Parent root = loader.load();
+			Stage stage = (Stage) burgerBox.getScene().getWindow();
+			stage.setScene(new Scene(root, 950, 800));
+			stage.setTitle(title);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void changeScene(StackPane stackPane, String file, String title) {
+		hover(stackPane);
+		stackPane.setOnMouseClicked(e -> {
+			//changeColor(stackPane);
+			loadScene(file, title);
+		});
+	}
 
 	@FXML
-	protected void onHelloButtonClick() {
-		welcomeText.setText("Welcome to JavaFX Application!");
+	public void goToCart(ActionEvent actionEvent) {
+		loadScene("CurrentOrder-view.fxml", "RU Burger - Cart");
 	}
+
+	@FXML
+	public void goToOrders(ActionEvent actionEvent) {
+		loadScene("PlacedOrder-view.fxml", "RU Burger - Orders");
+	}
+
+	public void initialize() {
+		changeScene(burgerBox, "Burger-view.fxml", "RU Burger - Ordering Burgers ");
+		changeScene(sandwichBox, "Sandwich-view.fxml", "RU Burger - Ordering Sandwiches");
+		changeScene(beverageBox, "Beverage-view.fxml", "RU Burger - Ordering Beverages");
+		changeScene(sideBox, "Side-view.fxml", "RU Burger - Ordering Sides");
+	}
+
+
 }
--- src/main/java/com/example/project4ruburger/model/Main.java
+++ src/main/java/com/example/project4ruburger/model/Main.java
@@ -16,8 +16,8 @@ public class Main extends Application {
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/example/project4ruburger/Main-view.fxml"));
 		Parent root = fxmlLoader.load();
-		Scene scene = new Scene(root, 320, 240);
-		stage.setTitle("Hello!");
+		Scene scene = new Scene(root, 950, 800);
+		stage.setTitle("RU Burger - Main Menu");
 		stage.setScene(scene);
 		stage.show();
 	}
